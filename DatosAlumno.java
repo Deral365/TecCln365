df05d1b0d5af0413d76ab20e806ba66a8532123e
PerezDeral365 <124410553+Deral365@users.noreply.github.com>
2024-09-06 15:44:53 -0700

    06/09/2024

---
+++ DatosAlumno.java
@@ -0,0 +1,9 @@
+public class DatosAlumno {
+    public static void main(String[] args) {
+        System.out.println("Angel Eduardo Perez Rojas 🥶");
+        System.out.println("24170682");
+        System.out.println("Ingenería en Sistemas Computacionales");
+        System.out.println("18 años");
+
+    }
+}
