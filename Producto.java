fd9ebdfe99ac91c9602a2353f32cbdbabfe32439
PerezDeral365 <124410553+Deral365@users.noreply.github.com>
2025-02-28 12:01:45 -0700

    sfggd

--- Producto.java
+++ Producto.java
@@ -1,10 +1,24 @@
 package Clases;
 
-public class Producto {
+public class Producto extends Productos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Productos A [] = new Productos[10];
+        Recolectar(A);
+        Iimprimir(A);
+       
 	}
+	 public static void Recolectar (Productos A []) {
+     A[0]=new Frescos("24/07/2025","352423","","");
+     A[1]=new Frescos("24/08/2025","352344","","");
+     
+     A[2]=new Refrigerados("24/08/2025","352344","","");
+     A[3]=new Refrigerados("24/08/2025","352344","","");
+     A[4]=new Refrigerados("24/08/2025","352344","","");
+	
+	 }
+	
+	
+	
 
 }
